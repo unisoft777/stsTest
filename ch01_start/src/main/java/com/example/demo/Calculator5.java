7faae85be7ed0505c8126a831ae938d05572c226
minsu03181213 <happy03181213@gmail.com>
2023-03-08 16:10:35 +0900

    6th

--- ch01_start/src/main/java/com/example/demo/Calculator5.java
+++ ch01_start/src/main/java/com/example/demo/Calculator5.java
@@ -3,7 +3,7 @@ package com.example.demo;
 public class Calculator4 {
 
 	public int sum(int a,int b) {
-		System.out.println("5번째 버전입니다.");
+		System.out.println("6번째 버전입니다.");
 		return a+b;
 	}
 }
